9b6e52a9d9c33205df48c2e68f9a00e59e7a2639
AgusFiorda <agus.fiorda.99@gmail.com>
2022-09-26 17:52:04 -0300

    finished Menu

--- primerParcial/darkMode.js
+++ primerParcial/darkMode.js
@@ -1,5 +1,10 @@
 const bdark = document.querySelector("#bdark");
 const body = document.querySelector("body");
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+
 bdark.addEventListener("click", (e) => {
   body.classList.toggle("darkmode");
+  header.classList.toggle("darkmode");
+  footer.classList.toggle("darkmode");
 });
